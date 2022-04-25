98e52b95e78a6fcb4b5626c7c5d9ff7ecb76fba4
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 14:04:14 +0200

    jobs listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 // import { useState } from 'react'
-import './App.scss'
+import "./App.scss";
+import ReactDOM from "react-dom";
+import jobs from "./data/jobs.json";
 
 function App() {
-  
-
   return (
     <div className="App">
-      hallo
+      <h1>Job Application Process</h1>
+      <div className="jobs">
+        {jobs.map((job, index) => {
+          return (
+            <div key={index} className="job">
+              <div className="position">
+                {" "}
+                <a href={job.url}>{job.position} </a>
+              </div>
+              <div className="skills"> {job.skills}</div>
+              <div className="bulkText">{job.bulkText}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
