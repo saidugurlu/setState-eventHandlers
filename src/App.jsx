5353fcbd0b26d086ab4cfbea3f14ff8b85b0b588
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 18:35:36 +0200

    jobs have status "send"

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-// import { useState } from 'react'
+import { useState } from "react";
 import "./App.scss";
 import ReactDOM from "react-dom";
-import jobs from "./data/jobs.json";
+import _jobs from "./data/jobs.json";
+//import card from "./components/card"
+
+_jobs.forEach(job => {
+  job.status = "send"
+})
+
 
 function App() {
+  const [displayKind, setDisplayKind] = useState("full");
+  const [jobs, setJobs] = useState(_jobs);
+  console.log(jobs);
+  const handleToggleView = () => {
+    setDisplayKind(displayKind === "full" ? "list" : "full");
+  };
   return (
     <div className="App">
       <h1>Job Application Process</h1>
-      <div className="jobs">
-        {jobs.map((job, index) => {
-          return (
-            <div key={index} className="job">
-              <div className="position">
-                {" "}
-                <a href={job.url}>{job.position} </a>
+      <button onClick={handleToggleView}>Toggle View</button>
+      {displayKind === "full" ? (
+        <div className="jobs">
+          {jobs.map((job, index) => {
+            return (
+              <div key={index} className="job">
+                <div className="position">
+                  {" "}
+                  <a href={job.url}>{job.position} </a>
+                </div>
+                <div className="skills"> {job.skills}</div>
+                <div className="bulkText">{job.bulkText}</div>
               </div>
-              <div className="skills"> {job.skills}</div>
-              <div className="bulkText">{job.bulkText}</div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <ul className="jobList">
+          {jobs.map((job, index) => {
+            return (
+              <li key={index} className="job">
+                <a href={job.url}>{job.position}</a>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
