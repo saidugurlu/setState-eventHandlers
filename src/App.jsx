d0d2747d8f368d666f806c08c5a5b73662ac704a
saidugurlu <ugurlumsaid@gmx.de>
2022-04-25 19:17:24 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -43,16 +43,24 @@ function App() {
         <div className="jobs">
           {jobs.map((job, index) => {
             return (
-              <div key={index} className="job">
+              <div key={index} className={`job ${job.status}`}>
                 <div className="header">
                   <div className="position">
-                    {" "}
                     <a href={job.url}>{job.position} </a>
                   </div>
-                  <button onClick={() => handleStatusChange(job)} className="status">{job.status}</button>
+                  <button
+                    onClick={() => handleStatusChange(job)}
+                    className="status"
+                  >
+                    {job.status}
+                  </button>
                 </div>
                 <div className="skills"> {job.skills}</div>
-                <div className="bulkText">{job.bulkText}</div>
+                {job.status !== 'declined' && (
+									<div className="bulkText">
+										{job.bulkText}
+									</div>
+								)}
               </div>
             );
           })}
